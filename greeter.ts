4aa961ceacc0176219aabaf3fd2d16ff6b32aeb2
zhangqd <zhangqd@digiwin.com>
2022-12-23 11:34:46 +0800

    支持在vscode中运行ts文件

--- greeter.ts
+++ greeter.ts
@@ -2,5 +2,8 @@ const str:String = '123'
 function greeter(person:String):String {
     return "Hello, " + person;
 }
-
+console.log(12)
+console.log(global)
 let user = "Jane User";
+
+const num = 123
