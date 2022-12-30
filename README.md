0767d212f5791309c4455e1ac35cda4b3ec47272
zhangqd <zhangqd@digiwin.com>
2022-12-30 16:00:24 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # typescript-study
 学习使用typescript
+
+## 1. 在vscode中直接运行ts文件
+- 安装依赖
+```bash
+npm install typescript ts-node -g
+```
+```bash
+pnpm install @types/node
+ typescript ts-node -g
+```
+- vscode安装Code Runner插件
+- 点击右上角运行按钮
--- greeter.ts
+++ greeter.ts
@@ -1,9 +1,12 @@
+const isDone:boolean = false
+const isDone1:Boolean = true
+
+console.log(isDone)
 const str:String = '123'
 function greeter(person:String):String {
     return "Hello, " + person;
 }
 console.log(12)
-console.log(global)
 let user = "Jane User";
 
 const num = 123
